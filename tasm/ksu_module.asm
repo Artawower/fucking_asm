71b3c61fea25675bffdf76924ef44e47149732c7
artawower <artawower@mail.ru>
2017-05-24 15:13:07 +0300

    fix ksu lab 3

---
+++ tasm/ksu_module.asm
@@ -0,0 +1,29 @@
+public equal_100
+code segment
+assume cs: code
+equal_100 proc near
+  ;результат возвращается в dx
+  ;портит cx, si, dx
+  push bp
+  mov bp, sp
+  mov ax, [bp + 4]
+  mov bx, [bp + 6]
+  add ax, bx
+  cmp ax, 100d
+  je yep
+  mov dx, nope
+  jmp blink_point
+  yep:
+    mov dx, yep
+
+  blink_point:
+  pop bp
+  ret 4
+endp equal
+code ends
+
+data Segment public
+yep       db 'Equal!', 13,10,'$'
+nope      db 'Not Equal!!1', 13,10,'$'
+data ends
+end
